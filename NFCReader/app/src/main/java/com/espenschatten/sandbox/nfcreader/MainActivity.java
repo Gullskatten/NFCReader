d03d992b30bf5aa12d427847376a837d8635db7f
Espen Gudmundsen <espen_gud@msn.com>
2016-03-02 19:29:17 +0100

    Now displaying NFC-icon, with animation. Also asks for permission to use NFC-technology.

--- NFCReader/app/src/main/java/com/espenschatten/sandbox/nfcreader/MainActivity.java
+++ NFCReader/app/src/main/java/com/espenschatten/sandbox/nfcreader/MainActivity.java
@@ -2,12 +2,26 @@ package com.espenschatten.sandbox.nfcreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mNfcIconImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initImageNfcAnimation();
     }
+
+    private void initImageNfcAnimation() {
+        mNfcIconImageView = (ImageView) findViewById(R.id.nfc_icon_white);
+        Animation pulse = AnimationUtils.loadAnimation(this, R.anim.scale);
+        mNfcIconImageView.startAnimation(pulse);
+    }
+
 }
